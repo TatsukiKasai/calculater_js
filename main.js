11f02e392acb349e047fb5d1e6c4779cfa7fbfd9
EC2 Default User <ec2-user@ip-172-31-34-170.ap-northeast-1.compute.internal>
2021-08-06 06:02:21 +0000

    second commit

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
+let display  = document.getElementById("display");
+
+function edit(elem){
+  if(display.value === "0"){
+    display.value = elem.value;
+  }else if(display.value === "00"){
+    display.value =  elem.value;
+  }else{
+    display.value = display.value + elem.value;
+  }
+}
 
 
-function get_calc(btn) {
-      if(btn.value == "=") {
-        document.calculater.display.value = eval(document.calculater.display.value);
-      } else if (btn.value == "AC") {
-        document.calculater.display.value = "";
-      } else{
-        document.calculater.display.value += btn.value;
+function calc(){
+  display.value = new Function("return " + display.value)();
+}
 
-      }
-    }
-     
+function calculation(elem) {
+  if(display.value.slice(-1) == "+"){
+    const l = display.value.slice(0,-1);
+    display.value = l + elem.value;
+  }else if(display.value.slice(-1) == "-"){
+    const l = display.value.slice(0,-1);
+    display.value = l + elem.value;
+  }else if(display.value.slice(-1) == "*"){
+    const l = display.value.slice(0,-1);
+    display.value = l + elem.value;
+  }else if(display.value.slice(-1) == "/"){
+    const l = display.value.slice(0,-1);
+    display.value = l + elem.value;
+  }else{
+    display.value = display.value + elem.value;
+  }
+}
+
+function update() {
+  display.value = "";
+}
